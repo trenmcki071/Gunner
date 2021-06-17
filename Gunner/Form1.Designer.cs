1bb13b0c65f587846d7fe9e60fea76054843cfec
trenmcki071 <trenmcki071@outlook.com>
2021-06-17 12:34:14 -0400

    Add project files.

---
+++ Gunner/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace Gunner
+{
+    partial class gunnerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(gunnerForm));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player1ScoreLabel = new System.Windows.Forms.Label();
+            this.player2ScoreLabel = new System.Windows.Forms.Label();
+            this.screen = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.explinationLabel = new System.Windows.Forms.Label();
+            this.explinationLabel2 = new System.Windows.Forms.Label();
+            this.explinationLabel1 = new System.Windows.Forms.Label();
+            this.explinationLabel3 = new System.Windows.Forms.Label();
+            this.boostLabel = new System.Windows.Forms.Label();
+            this.boostLabel1 = new System.Windows.Forms.Label();
+            this.explinationLabel5 = new System.Windows.Forms.Label();
+            this.explinationLabel4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 30;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // player1ScoreLabel
+            // 
+            this.player1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.player1ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.player1ScoreLabel.Location = new System.Drawing.Point(12, 9);
+            this.player1ScoreLabel.Name = "player1ScoreLabel";
+            this.player1ScoreLabel.Size = new System.Drawing.Size(188, 23);
+            this.player1ScoreLabel.TabIndex = 0;
+            this.player1ScoreLabel.Text = "0";
+            this.player1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // player2ScoreLabel
+            // 
+            this.player2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.player2ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.player2ScoreLabel.Location = new System.Drawing.Point(400, 9);
+            this.player2ScoreLabel.Name = "player2ScoreLabel";
+            this.player2ScoreLabel.Size = new System.Drawing.Size(188, 23);
+            this.player2ScoreLabel.TabIndex = 1;
+            this.player2ScoreLabel.Text = "0";
+            this.player2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // screen
+            // 
+            this.screen.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.screen.ForeColor = System.Drawing.Color.White;
+            this.screen.Location = new System.Drawing.Point(-1, -1);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(601, 403);
+            this.screen.TabIndex = 2;
+            this.screen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.AllowDrop = true;
+            this.startButton.FlatAppearance.BorderSize = 4;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.Color.Transparent;
+            this.startButton.Location = new System.Drawing.Point(235, 315);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(130, 50);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // explinationLabel
+            // 
+            this.explinationLabel.ForeColor = System.Drawing.Color.White;
+            this.explinationLabel.Location = new System.Drawing.Point(12, 32);
+            this.explinationLabel.Name = "explinationLabel";
+            this.explinationLabel.Size = new System.Drawing.Size(188, 23);
+            this.explinationLabel.TabIndex = 4;
+            this.explinationLabel.Text = "PLAYER 1";
+            this.explinationLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // explinationLabel2
+            // 
+            this.explinationLabel2.ForeColor = System.Drawing.Color.White;
+            this.explinationLabel2.Location = new System.Drawing.Point(58, 88);
+            this.explinationLabel2.Name = "explinationLabel2";
+            this.explinationLabel2.Size = new System.Drawing.Size(100, 44);
+            this.explinationLabel2.TabIndex = 5;
+            this.explinationLabel2.Text = "Use WASD to move and 1 to shoot";
+            this.explinationLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // explinationLabel1
+            // 
+            this.explinationLabel1.ForeColor = System.Drawing.Color.White;
+            this.explinationLabel1.Location = new System.Drawing.Point(403, 32);
+            this.explinationLabel1.Name = "explinationLabel1";
+            this.explinationLabel1.Size = new System.Drawing.Size(185, 23);
+            this.explinationLabel1.TabIndex = 6;
+            this.explinationLabel1.Text = "PLAYER 2";
+            this.explinationLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // explinationLabel3
+            // 
+            this.explinationLabel3.ForeColor = System.Drawing.Color.White;
+            this.explinationLabel3.Location = new System.Drawing.Point(445, 88);
+            this.explinationLabel3.Name = "explinationLabel3";
+            this.explinationLabel3.Size = new System.Drawing.Size(100, 44);
+            this.explinationLabel3.TabIndex = 7;
+            this.explinationLabel3.Text = "Use arrows to move and M to shoot";
+            this.explinationLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // boostLabel
+            // 
+            this.boostLabel.BackColor = System.Drawing.Color.Red;
+            this.boostLabel.Location = new System.Drawing.Point(235, 279);
+            this.boostLabel.Name = "boostLabel";
+            this.boostLabel.Size = new System.Drawing.Size(10, 10);
+            this.boostLabel.TabIndex = 8;
+            // 
+            // boostLabel1
+            // 
+            this.boostLabel1.BackColor = System.Drawing.Color.Yellow;
+            this.boostLabel1.Location = new System.Drawing.Point(235, 239);
+            this.boostLabel1.Name = "boostLabel1";
+            this.boostLabel1.Size = new System.Drawing.Size(10, 10);
+            this.boostLabel1.TabIndex = 9;
+            // 
+            // explinationLabel5
+            // 
+            this.explinationLabel5.BackColor = System.Drawing.Color.Transparent;
+            this.explinationLabel5.ForeColor = System.Drawing.Color.White;
+            this.explinationLabel5.Location = new System.Drawing.Point(264, 232);
+            this.explinationLabel5.Name = "explinationLabel5";
+            this.explinationLabel5.Size = new System.Drawing.Size(73, 23);
+            this.explinationLabel5.TabIndex = 10;
+            this.explinationLabel5.Text = "Speed Boost";
+            this.explinationLabel5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // explinationLabel4
+            // 
+            this.explinationLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.explinationLabel4.ForeColor = System.Drawing.Color.White;
+            this.explinationLabel4.Location = new System.Drawing.Point(264, 274);
+            this.explinationLabel4.Name = "explinationLabel4";
+            this.explinationLabel4.Size = new System.Drawing.Size(101, 23);
+            this.explinationLabel4.TabIndex = 11;
+            this.explinationLabel4.Text = "Pellet Speed Boost";
+            this.explinationLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gunnerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.explinationLabel4);
+            this.Controls.Add(this.explinationLabel5);
+            this.Controls.Add(this.boostLabel1);
+            this.Controls.Add(this.boostLabel);
+            this.Controls.Add(this.explinationLabel3);
+            this.Controls.Add(this.explinationLabel1);
+            this.Controls.Add(this.explinationLabel2);
+            this.Controls.Add(this.explinationLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.screen);
+            this.Controls.Add(this.player2ScoreLabel);
+            this.Controls.Add(this.player1ScoreLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(600, 400);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "gunnerForm";
+            this.Text = "Gunner";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.gunnerForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.gunnerForm_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.gunnerForm_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label player1ScoreLabel;
+        private System.Windows.Forms.Label player2ScoreLabel;
+        private System.Windows.Forms.Label screen;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label explinationLabel;
+        private System.Windows.Forms.Label explinationLabel2;
+        private System.Windows.Forms.Label explinationLabel1;
+        private System.Windows.Forms.Label explinationLabel3;
+        private System.Windows.Forms.Label boostLabel;
+        private System.Windows.Forms.Label boostLabel1;
+        private System.Windows.Forms.Label explinationLabel5;
+        private System.Windows.Forms.Label explinationLabel4;
+    }
+}
+
---
+++ Gunner/Form1.cs
@@ -0,0 +1,440 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Gunner
+{
+    public partial class gunnerForm : Form
+    {
+        Rectangle player1 = new Rectangle(10, 195, 40, 30);
+        Rectangle player2 = new Rectangle(550, 195, 40, 30);
+        Rectangle barrier = new Rectangle(295, 180, 10, 40);
+        Rectangle rightSpeedBoost = new Rectangle(420, 95, 10, 10);
+        Rectangle rightPelletBoost = new Rectangle(420, 295, 10, 10);
+        Rectangle leftSpeedBoost = new Rectangle(170, 95, 10, 10);
+        Rectangle leftPelletBoost = new Rectangle(170, 295, 10, 10);
+
+        List<Rectangle> pellets = new List<Rectangle>();
+        List<int> pelletSpeeds = new List<int>();
+
+        Image arrowLeft = Properties.Resources.arrowLeft;
+        Image arrowRight = Properties.Resources.arrowRight;
+
+        bool wDown = false;
+        bool aDown = false;
+        bool sDown = false;
+        bool dDown = false;
+        bool rightArrowDown = false;
+        bool leftArrowDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool leftShootDown = false;
+        bool rightShootDown = false;
+
+        int player1Speed = 1;
+        int player2Speed = 1;
+        int player1PelletSpeed = 4;
+        int player2PelletSpeed = -4;
+        int barrierSpeed = 3;
+
+        Pen whitePen = new Pen(Color.White);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        String boost;
+
+        Random rnd = new Random();
+
+        int leftdelay1;
+        int leftdelay2;
+        int rightdelay1;
+        int rightdelay2;
+
+        int leftFireRate = 40;
+        int rightFireRate = 40;
+
+        int rightShotLag = 0;
+        int leftShotLag = 0;
+
+        int player1Score = 0;
+        int player2Score = 0;
+        public gunnerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void gunnerForm_Paint(object sender, PaintEventArgs e)
+        {
+            //draw/fill the players, barriers, and boosts
+            e.Graphics.DrawImage(arrowLeft, player1);
+            e.Graphics.DrawImage(arrowRight, player2);
+            e.Graphics.DrawRectangle(whitePen, barrier);
+            e.Graphics.FillRectangle(yellowBrush, rightSpeedBoost);
+            e.Graphics.FillRectangle(redBrush, rightPelletBoost);
+            e.Graphics.FillRectangle(yellowBrush, leftSpeedBoost);
+            e.Graphics.FillRectangle(redBrush, leftPelletBoost);
+
+            //fill the pellets in the list
+            for (int i = 0; i < pellets.Count(); i++) 
+            {
+                e.Graphics.FillRectangle(whiteBrush, pellets[i]);
+            }
+
+            e.Graphics.DrawLine(whitePen, 201, 0, 201, 400);
+            e.Graphics.DrawLine(whitePen, 399, 0, 399, 400);
+        }
+
+        private void gunnerForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            //detects the button pushes
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.D1:
+                    leftShootDown = true;
+                    break;
+                case Keys.M:
+                    rightShootDown = true;
+                    break;
+            }
+        }
+
+        private void gunnerForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            //detects for the buttons not being pushed
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+                case Keys.M:
+                    rightShootDown = false;
+                    break;
+                case Keys.D1:
+                    leftShootDown = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //if a boost is activated, calls the method and lets it know which boost was activated
+            if (player1.IntersectsWith(leftSpeedBoost))
+            {
+                boost = "leftSpeed";
+                SpeedBoost();
+            }
+
+            if (player1.IntersectsWith(leftPelletBoost))
+            {
+                boost = "leftPellet";
+                SpeedBoost();
+            }
+
+            if (player2.IntersectsWith(rightSpeedBoost))
+            {
+                boost = "rightSpeed";
+                SpeedBoost();
+            }
+
+            if (player2.IntersectsWith(rightPelletBoost))
+            {
+                boost = "rightPellet";
+                SpeedBoost();
+            }
+
+            //shooting pellets, the lag number increases and once it gets bigger than the fire rate, another shot can be fired
+            rightShotLag++;
+            if (rightShootDown == true && rightShotLag > rightFireRate)
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.shot);
+                player.Play();
+                pellets.Add(new Rectangle(player2.X - 7, player2.Y + 14, 10, 3));
+                pelletSpeeds.Add(player2PelletSpeed);
+                rightShotLag = 0;
+            }
+
+            leftShotLag++;
+            if (leftShootDown == true && leftShotLag > leftFireRate)
+            {
+                SoundPlayer player = new SoundPlayer(Properties.Resources.shot);
+                player.Play();
+                pellets.Add(new Rectangle(player1.X + 37, player1.Y + 14, 10, 3));
+                pelletSpeeds.Add(player1PelletSpeed);
+                leftShotLag = 0;
+            }
+
+            //moving the pellets
+            for (int i = 0; i < pellets.Count(); i++)
+            {
+                int x = pellets[i].X + pelletSpeeds[i];
+
+                pellets[i] = new Rectangle(x, pellets[i].Y, 10, 3);
+            }
+
+            //check if a pellet had left the screen
+            for (int i = 0; i < pellets.Count(); i++)
+            {
+
+
+                if (pellets[i].X > this.Width || pellets[i].X - 15 > this.Width)
+                {
+                    pellets.RemoveAt(i);
+                    pelletSpeeds.RemoveAt(i);
+                }
+            }
+
+            //if a player is hit by a pellet
+            for (int i = 0; i < pellets.Count(); i++)
+            {
+                if (player1.IntersectsWith(pellets[i]))
+                {
+                    SoundPlayer player = new SoundPlayer(Properties.Resources.Hit);
+                    player.Play();
+                    pellets.RemoveAt(i);
+                    pelletSpeeds.RemoveAt(i);
+                    player2Score++;
+                    player2ScoreLabel.Text = $"{player2Score}";
+                }
+
+            }
+
+            for (int i = 0; i < pellets.Count(); i++)
+            {
+                if (player2.IntersectsWith(pellets[i]))
+                {
+                    SoundPlayer player = new SoundPlayer(Properties.Resources.Hit);
+                    player.Play();
+                    pellets.RemoveAt(i);
+                    pelletSpeeds.RemoveAt(i);
+                    player1Score++;
+                    player1ScoreLabel.Text = $"{player1Score}";
+                }
+
+            }
+            //checks for a game over
+            if (player1Score == 3)
+            {
+                gameTimer.Enabled = false;
+                screen.Visible = true;
+                screen.Text = "Player 1 Wins!!";
+            }
+
+            if (player2Score == 3)
+            {
+                gameTimer.Enabled = false;
+                screen.Visible = true;
+                screen.Text = "Player 2 Wins!!";
+            }
+
+            //if a pellet hits the barrier
+            for (int i = 0; i < pellets.Count(); i++)
+            {
+                if (barrier.IntersectsWith(pellets[i]))
+                {
+                    SoundPlayer player = new SoundPlayer(Properties.Resources.Hit);
+                    player.Play();
+                    pellets.RemoveAt(i);
+                    pelletSpeeds.RemoveAt(i);
+                }
+
+            }
+
+            //movement
+            if (wDown == true && player1.Y > 0)
+            {
+                player1.Y -= player1Speed;
+            }
+
+            if (sDown == true && player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += player1Speed;
+            }
+
+            if (aDown == true && player1.X > 0)
+            {
+                player1.X -= player1Speed;
+            }
+
+            if (dDown == true && player1.X < this.Width - player1.Width && player1.X < 200 - player1.Width)
+            {
+                player1.X += player1Speed;
+            }
+
+            if (upArrowDown == true && player2.Y > 0)
+            {
+                player2.Y -= player2Speed;
+            }
+
+            if (downArrowDown == true && player2.Y < this.Height - player2.Height)
+            {
+                player2.Y += player2Speed;
+            }
+
+            if (rightArrowDown == true && player2.X < this.Width - player2.Width)
+            {
+                player2.X += player2Speed;
+            }
+
+            if (leftArrowDown == true && player2.X > 0 && player2.X > 400)
+            {
+                player2.X -= player2Speed;
+            }
+
+            //if the boosts arent in their starting point, they slowly move towards it
+            if (leftSpeedBoost.X > 170)
+            {
+                leftdelay1++;
+                if (leftdelay1 > 15)
+                {
+                    leftSpeedBoost.X -= 1;
+                    leftdelay1 = 1;
+                }
+            }
+
+            if (leftPelletBoost.X > 170)
+            {
+                leftdelay2++;
+                if (leftdelay2 > 5)
+                {
+                    leftPelletBoost.X -= 1;
+                    leftdelay2 = 1;
+                }
+            }
+
+            if (rightPelletBoost.X < 420)
+            {
+                rightdelay2++;
+                if (rightdelay2 > 5)
+                {
+                    rightPelletBoost.X += 1;
+                    rightdelay2 = 1;
+                }
+            }
+
+            if (rightSpeedBoost.X < 420)
+            {
+                rightdelay1++;
+                if (rightdelay1 > 15)
+                {
+                    rightSpeedBoost.X += 1;
+                    rightdelay1 = 1;
+                }
+            }
+
+            //if the barrier hits a wall it'll reverse direction
+            barrier.Y += barrierSpeed;
+            if (barrier.Y == 0 || barrier.Y == this.Height - barrier.Height)
+            {
+                barrierSpeed = -barrierSpeed;
+            }
+
+            Refresh();
+        }
+
+        //this method is called when a player gets a power-up
+        public void SpeedBoost()
+        {
+            //a sound is played, and they are moved into the middle of the screen
+            SoundPlayer player = new SoundPlayer(Properties.Resources.power_up);
+            if (boost == "leftSpeed")
+            {
+                player.Play();
+                leftSpeedBoost.X = 280;
+                player1Speed++;
+            }
+
+            if (boost =="rightSpeed")
+            {
+                player.Play();
+                rightSpeedBoost.X = 310;
+                player2Speed++;
+            }
+
+            if (boost == "leftPellet")
+            {
+                player.Play();
+                leftPelletBoost.X = 280;
+                player1PelletSpeed++;
+                leftFireRate = leftFireRate - 5;
+            }
+
+            if (boost == "rightPellet")
+            {
+                player.Play();
+                rightPelletBoost.X = 310;
+                player2PelletSpeed--;
+                rightFireRate = rightFireRate - 5;
+            }
+        }
+
+        //starts the game once the start button is pressed
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            gameTimer.Enabled = true;
+            startButton.Enabled = false;
+            screen.Visible = false;
+            explinationLabel.Visible = false;
+            explinationLabel1.Visible = false;
+            explinationLabel2.Visible = false;
+            explinationLabel3.Visible = false;
+            explinationLabel4.Visible = false;
+            explinationLabel5.Visible = false;
+            boostLabel.Visible = false;
+            boostLabel1.Visible = false;
+            startButton.Visible = false;
+        }
+    }
+}
